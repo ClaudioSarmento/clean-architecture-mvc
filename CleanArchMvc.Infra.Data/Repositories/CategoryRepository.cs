a90d4e00d1c15766cbcef0cd4b9ffc36d98dfa01
ClaudioSarmento <claudio-s-i@hotmail.com>
2026-03-12 21:54:55 -0300

    Implementando o repositório CategoryRepository

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Domain.Validation;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private ApplicationDbContext _categoryContext;
+
+    public  CategoryRepository(ApplicationDbContext context) {  _categoryContext = context; }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        _categoryContext.Categories.Add(category);
+        await _categoryContext.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> GetByIdAsync(int? id)
+    {
+        var category = await _categoryContext.Categories.FindAsync(id);
+        DomainExceptionValidation.When(category == null, "Categoria não encontrada");
+        return category!;
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesAsync()
+    {
+        var categories = await _categoryContext.Categories
+            .AsNoTracking()
+            .ToListAsync();
+        return categories;
+    }
+
+    public async Task<Category> RemoveAsync(Category category)
+    {
+        _categoryContext.Remove(category);
+        await _categoryContext.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> UpdateAsync(Category category)
+    {
+        _categoryContext.Update(category);
+        await _categoryContext.SaveChangesAsync();
+        return category;
+    }
+}
