bf14945990794b6b4f88237c138ff5e1c0f5bc46
ClaudioSarmento <claudio-s-i@hotmail.com>
2026-03-26 20:25:34 -0300

    Teste - Criando Controller e View para Product

---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+namespace CleanArchMvc.WebUI.Controllers;
+
+public class ProductsController : Controller
+{
+    private readonly IProductService _productService;
+
+    public ProductsController(IProductService productService)
+    {
+        _productService = productService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var result = await _productService.GetProductsAsync();
+        return View(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        var result = await _productService.GetProductByIdAsync(id);
+        return View(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(ProductDTO productDTO)
+    {
+        await _productService.UpdateAsync(productDTO);
+        return RedirectToAction(nameof(Index));
+    }
+}
